920572f8c98a6c0021651c6630eb84e12ebe388c
Mateusz Ścisło <m.scislo@wp.pl>
2017-12-10 16:14:42 +0100

    Add and delete patients

--- PatientService.java
+++ PatientService.java
@@ -5,11 +5,12 @@ class PatientService {
     private final List<Patient> patients = new ArrayList<>();
 
     PatientService() {
-        this.patients.add(new Patient("Adam", "Nowak", 12));
         this.patients.add(new Patient("Maria", "Nowakowska", 14));
         this.patients.add(new Patient("Daria", "Polakowska", 15));
         this.patients.add(new Patient("Aria", "Nosowska", 16));
         this.patients.add(new Patient("Katarzyna", "Dworakowska", 17));
+        this.patients.add(new Patient("Mariola", "Marmolada", 19));
+        this.patients.add(new Patient("Katarzyna", "Zając", 17));
     }
 
     List<Patient> getAll() {
